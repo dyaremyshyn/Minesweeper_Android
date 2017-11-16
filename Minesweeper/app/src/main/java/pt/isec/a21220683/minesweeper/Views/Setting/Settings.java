095427c585a1205c2fc059320c9023c6adab2228
dyaremyshyn <dmytroyaremyshyn@gmail.com>
2017-11-16 17:09:16 +0000

    Bug Fixed

--- Minesweeper/app/src/main/java/pt/isec/a21220683/minesweeper/Views/Setting/Settings.java
+++ Minesweeper/app/src/main/java/pt/isec/a21220683/minesweeper/Views/Setting/Settings.java
@@ -37,16 +37,7 @@ public class Settings extends Activity {
         nomeJogador = (EditText) findViewById(R.id.edtNomeJogador);
         nomeJogador.setText(getNomeSP());
 
-        SharedPreferences sharedPreferences = getSharedPreferences("FotoJogador", Context.MODE_PRIVATE);
-
-        path = sharedPreferences.getString("foto","");
         foto = (ImageView) findViewById(R.id.foto);
-        File imgFile = new File(path);
-
-        if (imgFile.exists()){
-            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-            foto.setImageBitmap(myBitmap);
-        }
 
         Button creditos = (Button) findViewById(R.id.btnCreditos);
         creditos.setOnClickListener(new View.OnClickListener() {
@@ -62,62 +53,17 @@ public class Settings extends Activity {
             @Override
             public void onClick(View view) {
 
-                if (nomeJogador.getText().toString().isEmpty() == false) {
-                    SharedPreferences sharedPref = getSharedPreferences("NomeJogador", Context.MODE_PRIVATE);
 
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString("nome", nomeJogador.getText().toString());
-                    editor.apply();
 
-                    Toast.makeText(Settings.this, "Guardei nome", Toast.LENGTH_SHORT).show();
-
-                }
-                /*
-                if (foto != null){
-                    System.out.println("\n\nHa foto! ");
-                    Toast.makeText(Definicoes.this,"Tenho foto",Toast.LENGTH_SHORT).show();
-                    foto.buildDrawingCache();
-                    Bitmap bmap = foto.getDrawingCache();
-                    Uri uri = getImageUri(getApplicationContext(),bmap);
-                    path = getRealPathFromURI(uri);
-
-                    SharedPreferences sharedPref = getSharedPreferences("FotoJogador", Context.MODE_PRIVATE);
-                    System.out.println("\n\nVou guardar! ");
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString("foto", path);
-                    editor.apply();
-                }
-                */
             }
         });
 
     }
 
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
 
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
 
-    public void mudarFoto(View view){
-        dispatchTakePictureIntent();
-    }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
 
     public String getNomeSP(){
         SharedPreferences sharedPreferences = getSharedPreferences("NomeJogador",Context.MODE_PRIVATE);
@@ -131,6 +77,7 @@ public class Settings extends Activity {
         editor.putString("nome",nomeJogador.getText().toString());
         editor.apply();
 
-        Toast.makeText(this,"Guardado nome do jogador",Toast.LENGTH_SHORT).show();Toast.makeText(this,"Guardado nome do jogador",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Guardado nome do jogador",Toast.LENGTH_SHORT).show();
+
     }
 }
